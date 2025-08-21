b4ac30fd0c33f0f2ad4b0180af1d0b41c74bce2b
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2025-08-21 18:18:25 +0900

    Update ads1115.cpp

--- ads1115.cpp
+++ ads1115.cpp
@@ -2,18 +2,22 @@
 
 adc::adc(pins _pin):pin((int)_pin){
     conf[1]|=(pin<<4);
-    printf("%d",conf[1]);
+    // printf("%d",conf[1]);
 }
 
 void adc::init(){
-
+    i2c_init(i2c0, 100000);
+    gpio_set_function(4, GPIO_FUNC_I2C);
+    gpio_set_function(5, GPIO_FUNC_I2C);
+    gpio_pull_up(4);
+    gpio_pull_up(5);
 }
 
 int adc::read(){
     i2c.write(ADDR,conf,3);
     uint8_t result[2];
     uint8_t data=0b00000000;
-    i2c.write(ADDR,&data,1);
-    i2c.read(ADDR,result,2);
+    i2c_write_blocking(i2c0,ADDR,&data,1,true);
+    i2c_read_blocking(i2c0,ADDR,result,2,true);
     return (result[0]<<4)|(result[1]>>4);
 }
