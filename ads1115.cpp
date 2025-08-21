604dada6ef1f7057e2fa285c56c25b29f66e844e
hirobon1690 <hirobon1690@gmail.com>
2025-08-22 01:12:20 +0900

    update calib

--- ads1115.cpp
+++ ads1115.cpp
@@ -1,11 +1,17 @@
 #include "ads1115.h"
 
-adc::adc(pins _pin):pin((int)_pin){
-    conf[1]|=(pin<<4);
+Ads1115::Ads1115(pins _pin):pin((int)_pin){
+    conf[1] &= ~(0b111 << 4);
+    conf[1] |= ((pin & 0b111) << 4);
+    conf[1] &= ~(1 << 0);              // Configビット8=MODE（MSBバイトのbit0）
+
+    // DR = 860SPS（最速）。他のDRにしたい場合は値を変えてください。
+    conf[2] &= ~(0b111 << 5);          // DR[2:0]をクリア
+    conf[2] |=  (0b111 << 5);          // 111b = 860SPS
     // printf("%d",conf[1]);
 }
 
-void adc::init(){
+void Ads1115::init(){
     i2c_init(i2c0, 100000);
     gpio_set_function(4, GPIO_FUNC_I2C);
     gpio_set_function(5, GPIO_FUNC_I2C);
@@ -13,11 +19,17 @@ void adc::init(){
     gpio_pull_up(5);
 }
 
-int adc::read(){
+int Ads1115::read(){
     i2c_write_blocking(i2c0,ADDR,conf,3,true);
     uint8_t result[2];
     uint8_t data=0b00000000;
     i2c_write_blocking(i2c0,ADDR,&data,1,true);
+    sleep_ms(5);
     i2c_read_blocking(i2c0,ADDR,result,2,true);
-    return (int16_t)((result[0] << 8) | result[1]);
+    int raw =  (int16_t)((result[0] << 8) | result[1]);
+    return raw + cali;
+}
+
+int Ads1115::setCali(int cali){
+    this->cali = cali;
 }
--- ads1115.h
+++ ads1115.h
@@ -8,14 +8,16 @@ enum pins{
     A2=0b110,
     A3=0b111
 };
-class adc{
+class Ads1115{
     public:
-        adc(pins);
+        Ads1115(pins);
         void init();
         int read();
+        int setCali(int);
 
     private:
         int pin;
         uint8_t conf[3]={0b00000001,0b10000010,0b10000011};
         const uint8_t ADDR=0x48;
+        int cali=0;
 };
